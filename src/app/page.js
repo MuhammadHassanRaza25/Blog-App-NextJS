a861facb14130d7cdc479b217078a09d5660f5dd
Muhammad Hassan Raza <153998754+MuhammadHassanRaza25@users.noreply.github.com>
2025-10-19 20:37:19 +0500

    anmation added

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Hero from "@/components/Hero";
 import Header from "../components/Header";
 import Footer from "@/components/Footer";
 import BlogCard from "./components/BlogCard";
+import { MotionUp } from "@/components/ui/motion-up";
 
 export default async function Home() {
   let res = await fetch("http://localhost:3000/api/blogs");
@@ -13,26 +14,30 @@ export default async function Home() {
       <Hero />
 
       {/* Blog Posts Section */}
-       <div className="flex flex-col items-center justify-center overflow-hidden rounded-md pt-18">
-        <h1 className="font-semibold text-center lg:text-4xl md:text-2xl text-xl text-white pb-4">
-          Insights & Stories
-        </h1>
+      <MotionUp delay={0}>
+        <div className="flex flex-col items-center justify-center overflow-hidden rounded-md pt-18">
+          <h1 className="font-semibold text-center lg:text-4xl md:text-2xl text-xl text-white pb-4">
+            Insights & Stories
+          </h1>
 
-        <div className="w-[40rem] h-10 relative">
-          {/* Gradients */}
-          <div className="absolute inset-x-16 top-0 bg-gradient-to-r from-transparent via-emerald-500 to-transparent h-[2px] w-3/4 blur-sm" />
-          <div className="absolute inset-x-16 top-0 bg-gradient-to-r from-transparent via-emerald-500 to-transparent h-px w-3/4" />
-          <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-emerald-500 to-transparent h-[5px] w-1/4 blur-sm" />
-          <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-emerald-500 to-transparent h-px w-1/4" />
+          <div className="w-[40rem] h-10 relative">
+            {/* Gradients */}
+            <div className="absolute inset-x-16 top-0 bg-gradient-to-r from-transparent via-emerald-500 to-transparent h-[2px] w-3/4 blur-sm" />
+            <div className="absolute inset-x-16 top-0 bg-gradient-to-r from-transparent via-emerald-500 to-transparent h-px w-3/4" />
+            <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-emerald-500 to-transparent h-[5px] w-1/4 blur-sm" />
+            <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-emerald-500 to-transparent h-px w-1/4" />
 
-          {/* Radial Gradient to prevent sharp edges */}
-          <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+            {/* Radial Gradient to prevent sharp edges */}
+            <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+          </div>
         </div>
-      </div>
+      </MotionUp>
 
       <div className="mt-10 mb-18 flex flex-wrap gap-5 justify-center max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-        {res.data?.map((blogs) => (
-          <BlogCard key={blogs._id} data={blogs} />
+        {res.data?.map((blogs, index) => (
+          <MotionUp key={blogs._id}  delay={index * 0.1}>
+          <BlogCard data={blogs} />
+          </MotionUp>
         ))}
       </div>
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { FaPenNib } from "react-icons/fa6";
 import { HiOutlineLightBulb } from "react-icons/hi";
+import { MotionUp } from "./ui/motion-up";
 
 export default function Hero() {
   return (
@@ -17,39 +18,43 @@ export default function Hero() {
 
         {/* Hero Content Div */}
         <div className="flex flex-col gap-5 items-center justify-center max-w-7xl mx-auto px-4 pt-20 pb-20 sm:px-6 lg:px-8 relative z-10">
-          <div className="inline-flex gap-2 mt-1 items-center bg-white/10 backdrop-blur-md px-4 py-2 rounded-full border border-white/20 shadow-sm">
-            <span className="text-sm font-medium text-white drop-shadow-sm">
-              Thoughts, Stories & Ideas
-            </span>
-            <HiOutlineLightBulb className="w-4 h-4 text-yellow-400 mr-2 drop-shadow-sm" />
-          </div>
+            <div className="inline-flex gap-2 mt-1 items-center bg-white/10 backdrop-blur-md px-4 py-2 rounded-full border border-white/20 shadow-sm">
+              <span className="text-sm font-medium text-white drop-shadow-sm">
+                Thoughts, Stories & Ideas
+              </span>
+              <HiOutlineLightBulb className="w-4 h-4 text-yellow-400 mr-2 drop-shadow-sm" />
+            </div>
 
           {/* Heading and para */}
           <div className="flex flex-col items-center">
-            <h1 className="lg:text-5xl md:text-3xl text-2xl font-semibold text-white text-center">
-              Welcome to{" "}
-              <span className="font-bold bg-gradient-to-r from-emerald-600 to-emerald-400 bg-clip-text text-transparent">
-                GlassyBlog
-              </span>
-            </h1>
-            <p className="lg:w-[70%] md:w-[70%] w-[90%] lg:text-lg md:text-base text-sm text-center text-gray-100 mt-5">
-              Every day brings a new story, a fresh perspective! Dive into our
-              blogs to discover inspiring tips, heartfelt stories, and
-              everything that fuels your passion to chase your dreams. Join us
-              on this journey where every word is meant to touch your heart and
-              spark your curiosity.
-            </p>
+            <MotionUp delay={0.2}>
+              <h1 className="lg:text-5xl md:text-3xl text-2xl font-semibold text-white text-center">
+                Welcome to{" "}
+                <span className="font-bold bg-gradient-to-r from-emerald-600 to-emerald-400 bg-clip-text text-transparent">
+                  GlassyBlog
+                </span>
+              </h1>
+            </MotionUp>
+            <MotionUp delay={0.3} className="w-full">
+              <p className="lg:w-[70%] md:w-[70%] w-[90%] mx-auto lg:text-lg md:text-base text-sm text-center text-gray-100 mt-5">
+                Every day brings a new story, a fresh perspective! Dive into our
+                blogs to discover inspiring tips, heartfelt stories, and
+                everything that fuels your passion to chase your dreams. Join us
+                on this journey where every word is meant to touch your heart
+                and spark your curiosity.
+              </p>
+            </MotionUp>
           </div>
 
           {/* Button */}
-          <Link href="/blog/create">
-            <div className="group relative w-fit mx-auto">
-              <button className="flex gap-3 items-center px-4 py-2 font-semibold backdrop-blur-sm border bg-emerald-700/30 focus:outline-none focus:bg-emerald-700/40 hover:bg-emerald-700/40 border-emerald-500/50 hover:border-emerald-500 text-white mx-auto text-center rounded-full transition-all duration-300 cursor-pointer">
-                Create Your Blog <FaPenNib />
-                <span className="pointer-events-none absolute bottom-0 left-1/2 -translate-x-1/2 w-0 h-px bg-gradient-to-r from-transparent via-emerald-400 to-transparent transition-all duration-300 group-hover:w-3/4" />
-              </button>
-            </div>
-          </Link>
+            <Link href="/blog/create">
+              <div className="group relative w-fit mx-auto">
+                <button className="flex gap-3 items-center px-4 py-2 font-semibold backdrop-blur-sm border bg-emerald-700/30 focus:outline-none focus:bg-emerald-700/40 hover:bg-emerald-700/40 border-emerald-500/50 hover:border-emerald-500 text-white mx-auto text-center rounded-full transition-all duration-300 cursor-pointer">
+                  Create Your Blog <FaPenNib />
+                  <span className="pointer-events-none absolute bottom-0 left-1/2 -translate-x-1/2 w-0 h-px bg-gradient-to-r from-transparent via-emerald-400 to-transparent transition-all duration-300 group-hover:w-3/4" />
+                </button>
+              </div>
+            </Link>
         </div>
       </div>
     </>
