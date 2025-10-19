44a69850269013af05a833d0c641ff12f98e661e
Muhammad Hassan Raza <153998754+MuhammadHassanRaza25@users.noreply.github.com>
2025-10-19 20:46:56 +0500

    set responsiveness

--- src/app/page.js
+++ src/app/page.js
@@ -16,18 +16,18 @@ export default async function Home() {
       {/* Blog Posts Section */}
       <MotionUp delay={0}>
         <div className="flex flex-col items-center justify-center overflow-hidden rounded-md pt-18">
-          <h1 className="font-semibold text-center lg:text-4xl md:text-2xl text-xl text-white pb-4">
+          <h1 className="font-semibold text-center lg:text-4xl md:text-2xl text-2xl text-white pb-4">
             Insights & Stories
           </h1>
 
-          <div className="w-[40rem] h-10 relative">
+          <div className="relative w-full max-w-md sm:max-w-xl md:max-w-2xl lg:max-w-2xl h-10 mx-auto">
             {/* Gradients */}
-            <div className="absolute inset-x-16 top-0 bg-gradient-to-r from-transparent via-emerald-500 to-transparent h-[2px] w-3/4 blur-sm" />
-            <div className="absolute inset-x-16 top-0 bg-gradient-to-r from-transparent via-emerald-500 to-transparent h-px w-3/4" />
-            <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-emerald-500 to-transparent h-[5px] w-1/4 blur-sm" />
-            <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-emerald-500 to-transparent h-px w-1/4" />
+            <div className="absolute left-1/2 top-0 w-3/4 -translate-x-1/2 bg-gradient-to-r from-transparent via-emerald-500 to-transparent h-[2px] blur-sm" />
+            <div className="absolute left-1/2 top-0 w-3/4 -translate-x-1/2 bg-gradient-to-r from-transparent via-emerald-500 to-transparent h-px" />
+            <div className="absolute left-1/2 top-0 w-1/4 -translate-x-1/2 bg-gradient-to-r from-transparent via-emerald-500 to-transparent h-[5px] blur-sm" />
+            <div className="absolute left-1/2 top-0 w-1/4 -translate-x-1/2 bg-gradient-to-r from-transparent via-emerald-500 to-transparent h-px" />
 
-            {/* Radial Gradient to prevent sharp edges */}
+            {/* Radial mask to smooth edges */}
             <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
           </div>
         </div>
@@ -35,8 +35,8 @@ export default async function Home() {
 
       <div className="mt-10 mb-18 flex flex-wrap gap-5 justify-center max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
         {res.data?.map((blogs, index) => (
-          <MotionUp key={blogs._id}  delay={index * 0.1}>
-          <BlogCard data={blogs} />
+          <MotionUp key={blogs._id} delay={index * 0.1}>
+            <BlogCard data={blogs} />
           </MotionUp>
         ))}
       </div>
