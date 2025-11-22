b8f289a306a6da0047ad7d1da99df3dc0b8ad6b0
Muhammad Hassan Raza <153998754+MuhammadHassanRaza25@users.noreply.github.com>
2025-11-22 23:41:20 +0500

    Created edit, delete backend routes

--- src/app/api/myblogs/[id]/route.js
+++ src/app/api/myblogs/[id]/route.js
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 import { ConnectDB } from "@/app/lib/dbConnect";
 
 export async function GET(request, { params }) {
-  await ConnectDB()
+  await ConnectDB();
   const { id } = await params;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
@@ -30,6 +30,57 @@ export async function GET(request, { params }) {
   } catch (error) {
     return NextResponse.json(
       { data: null, msg: "Server Error", error: error.message },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PUT(request, { params }) {
+  await ConnectDB();
+  const { id } = params;
+  const body = await request.json(); // data for edit
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return NextResponse.json({ msg: "Invalid Blog ID" }, { status: 404 });
+  }
+
+  try {
+    const updateBlog = await BlogModel.findByIdAndUpdate(id, body, {
+      new: true,
+    });
+
+    if (!updateBlog) {
+      return NextResponse.json({ msg: "Blog not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ data: updateBlog, msg: "Blog updated" });
+  } catch (err) {
+    return NextResponse.json(
+      { msg: "Server Error", error: err.message },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(request, { params }) {
+  await ConnectDB();
+  const { id } = params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return NextResponse.json({ msg: "Invalid Blog ID" }, { status: 404 });
+  }
+
+  try {
+    const deleteBlog = await BlogModel.findByIdAndDelete(id);
+
+    if (!deleteBlog) {
+      return NextResponse.json({ msg: "Blog not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ msg: "Blog deleted successfully" });
+  } catch (err) {
+    return NextResponse.json(
+      { msg: "Server Error", error: err.message },
       { status: 500 }
     );
   }
