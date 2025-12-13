805f6a4c31653a42517f3e1c21a81e7d8519c396
Muhammad Hassan Raza <153998754+MuhammadHassanRaza25@users.noreply.github.com>
2025-12-13 22:32:42 +0500

    admin page protected

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,7 +1,16 @@
+import { redirect } from "next/navigation";
 import AdminDashboard from "./AdminDashboard"
+import { verifyUser } from "../lib/verifyUser";
 
-export default function AdminPage(){
-    // role logic here
+export default async function AdminPage(){
+
+    const user = await verifyUser();
+    if(!user || user.role !== "admin"){
+        redirect("/")
+    }
+    // console.log('Checking Role In Admin Page ===>', user.role);
+     
+
     return(
         <AdminDashboard/>
     )
